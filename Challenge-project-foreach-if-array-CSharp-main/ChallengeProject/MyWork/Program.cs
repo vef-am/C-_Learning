22b2982347fc579321ac8073c5367b837bebf812
vef-am <veref.caceres@gmail.com>
2025-07-13 19:11:48 +0100

    Update files

--- Challenge-project-foreach-if-array-CSharp-main/ChallengeProject/MyWork/Program.cs
+++ Challenge-project-foreach-if-array-CSharp-main/ChallengeProject/MyWork/Program.cs
@@ -33,7 +33,7 @@ string currentStudentLetterGrade = "";
 
 // display the header row for scores/grades
 Console.Clear();
-Console.WriteLine("Student\t\tGrade\tLetter Grade\n");
+Console.WriteLine("Student\t\tExam Score\tOverall Grade\tExtra Credit\n");
 
 /*
 The outer foreach loop is used to:
@@ -59,11 +59,18 @@ foreach (string name in studentNames)
     else if (currentStudent == "Logan")
         studentScores = loganScores;
 
-    int sumAssignmentScores = 0;
-
     decimal currentStudentGrade = 0;
 
     int gradedAssignments = 0;
+
+    int sumExamScores = 0;
+    decimal currentExamGrade = 0;
+
+    int sumExtraCreditsScores = 0;
+    decimal currentExtraCreditsScores = 0;
+
+    int gradedExtraCredits = 0;
+    decimal currentExtraCredits = 0;
 
     /* 
     the inner foreach loop sums assignment scores
@@ -74,13 +81,20 @@ foreach (string name in studentNames)
         gradedAssignments += 1;
 
         if (gradedAssignments <= examAssignments)
-            sumAssignmentScores += score;
-
+        {
+            sumExamScores += score;
+        }
         else
-            sumAssignmentScores += score / 10;
+        {
+            gradedExtraCredits++;
+            sumExtraCreditsScores += score;
+        }
     }
 
-    currentStudentGrade = (decimal)(sumAssignmentScores) / examAssignments;
+    currentStudentGrade = (decimal)((sumExamScores) + ((decimal)(sumExtraCreditsScores) / 10)) / examAssignments;
+    currentExamGrade = (decimal)(sumExamScores) / examAssignments;
+    currentExtraCreditsScores = ((decimal)(sumExtraCreditsScores) / 10) / examAssignments;
+    currentExtraCredits = (decimal)sumExtraCreditsScores / gradedExtraCredits;
 
     if (currentStudentGrade >= 97)
         currentStudentLetterGrade = "A+";
@@ -123,8 +137,8 @@ foreach (string name in studentNames)
 
     // Student         Grade
     // Sophia:         92.2    A-
-    
-    Console.WriteLine($"{currentStudent}\t\t{currentStudentGrade}\t{currentStudentLetterGrade}");
+
+    Console.WriteLine($"{currentStudent}\t\t{currentExamGrade}\t\t{currentStudentGrade}\t{currentStudentLetterGrade}\t{currentExtraCredits} ({currentExtraCreditsScores} pts)");
 }
 
 // required for running in VS Code (keeps the Output windows open to view results)
--- CsharpProjects/TestProject/obj/Debug/net9.0/TestProject.AssemblyInfo.cs
+++ CsharpProjects/TestProject/obj/Debug/net9.0/TestProject.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+af2b508ca352c4e73397e46558664fd253692d9d")]
 [assembly: System.Reflection.AssemblyProductAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
