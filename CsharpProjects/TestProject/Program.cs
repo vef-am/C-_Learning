8d474a87275034642db731f84e2fdd97fb2ec337
vef-am <veref.caceres@gmail.com>
2025-08-26 12:49:04 +0100

    Continuing course. Add Looping Logic to Your Code Using the do-while and while Statements in C#. Unit 5

--- CsharpProjects/TestProject/Program.cs
+++ CsharpProjects/TestProject/Program.cs
@@ -1,13 +1,25 @@
-﻿for (int i = 1; i <= 100; i++)
-{
-    Console.Write(i);
+﻿Console.WriteLine("Enter an integer value between 5 and 10");
+bool validNumber = false;
+string? readNumber;
+int resultNumber = 0;
 
-    if (i % 3 == 0 && i % 5 == 0)
-        Console.WriteLine(" - FizzBuzz");
-    else if (i % 3 == 0)
-        Console.WriteLine(" - Fizz");
-    else if (i % 5 == 0)
-        Console.WriteLine(" - Buzz");
-    else
-        Console.WriteLine();
-}
+do
+{
+    readNumber = Console.ReadLine();
+
+    if (readNumber == null) continue;
+
+    validNumber = int.TryParse(readNumber, out resultNumber);
+
+    if (!validNumber)
+    {
+        Console.WriteLine("Sorry, you entered an invalid number, please try again");
+    }
+    else if (resultNumber < 5 || resultNumber > 10)
+    {
+        Console.WriteLine($"You entered {resultNumber}. Please enter a number between 5 and 10.");
+        validNumber = false;
+    }
+} while (!validNumber);
+
+Console.WriteLine($"Your input value ({resultNumber}) has been accepted.");
