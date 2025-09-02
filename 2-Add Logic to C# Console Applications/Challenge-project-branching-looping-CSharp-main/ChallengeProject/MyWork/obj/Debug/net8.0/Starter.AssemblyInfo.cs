bdad773d131e7d8a5ee6e6e22a70f259bf41a7a4
vef-am <veref.caceres@gmail.com>
2025-09-02 16:36:14 +0100

    Finished: Convert Data Types Using Casting and Conversion Techniques in C#

--- 2-Add Logic to C# Console Applications/Challenge-project-branching-looping-CSharp-main/ChallengeProject/MyWork/obj/Debug/net8.0/Starter.AssemblyInfo.cs
+++ 2-Add Logic to C# Console Applications/Challenge-project-branching-looping-CSharp-main/ChallengeProject/MyWork/obj/Debug/net8.0/Starter.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Starter")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+81211344456cdfc5fd7b49c30f93665a3e3d2afe")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e5ebf05c48c532f5089c8ba8a9c3c0af6ee045af")]
 [assembly: System.Reflection.AssemblyProductAttribute("Starter")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Starter")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
